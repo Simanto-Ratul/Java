035ad2532a14c5fb45a453c66cf90207d119f98d
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-14 15:52:55 +0600

    Contructor Overloading

--- Java practice/Polymorphism/Practice 4/ConstractorOverloading.java
+++ Java practice/Polymorphism/Practice 4/ConstractorOverloading.java
@@ -1,3 +1,27 @@
 public class ConstractorOverloading {
-    
+    public int x;
+    public int y;
+
+    public ConstractorOverloading(){
+        System.out.println("Default contrutor called.");
+    }
+
+    public ConstractorOverloading(int x, int y){
+        System.out.println("2nd cons called.");
+    }
+
+    public ConstractorOverloading(int x, int y, double z){
+        System.out.println("3rd constructor called.");
+    }
+
+    public ConstractorOverloading(double x, float y, String z){
+        System.out.println("4th cons called.");
+    }
+
+    public static void main(String[] args) {
+        ConstractorOverloading c1 = new ConstractorOverloading();
+        ConstractorOverloading c2 = new ConstractorOverloading(7, 9);
+        ConstractorOverloading c3 = new ConstractorOverloading(5, 6 , 6.64);
+        ConstractorOverloading c4 = new ConstractorOverloading(7.65, 4.54f, "Simanto");
+    }
 }
