f9c3fd408c5bd8ef4b6ee3ff3464a2c7b3143e12
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-15 12:31:30 +0600

    Abstract class declear

--- Java practice/Abstraction/Practice 3/Bank.java
+++ Java practice/Abstraction/Practice 3/Bank.java
@@ -1,3 +1,50 @@
-public class Bank {
-    
+public abstract class Bank {
+    private String accountNum;
+    private String customerName;
+    private double balance;
+
+    public Bank(String accountNum, double balance) {
+        this.accountNum = accountNum;
+        this.balance = balance;
+    }
+
+    public Bank(String accountNum, String customerName, double balance) {
+        this.accountNum = accountNum;
+        this.customerName = customerName;
+        this.balance = balance;
+    }
+
+    public String getAccountNum() {
+        return accountNum;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void Deposit(int amount) {
+        balance += amount;
+        System.out.println("Amount " + amount + " deposited.");
+    }
+
+    public void Withdraw(int amount) {
+        if (balance > amount && amount > 0) {
+            balance -= amount;
+            System.out.println("Amount " + amount + " withdrawn.");
+        } else {
+            System.out.println("Insuficiant Balance.");
+        }
+    }
+
+    public abstract void  interest();
+
+    public void display() {
+        System.out.println("Account Number: " + getAccountNum());
+        System.out.println("Account Holder: " + getCustomerName());
+        System.out.println("Balance: " + getBalance());
+    }
 }
