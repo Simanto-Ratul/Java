1790f01d2ae27b1dccfccdc0b52b447f4d1e400d
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-16 12:25:51 +0600

    Abstraction

--- Java practice/Abstraction/Practice 4/Main.java
+++ Java practice/Abstraction/Practice 4/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        SavingsAccount s1 = new SavingsAccount("Simanto Saha", "444828", 30000.00, "123456789", 0.8);
+        s1.showDetails();
     }
 }
