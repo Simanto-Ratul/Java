4c361676aa2f6ba3ac5ffb52afe3d22a3e96e3b4
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-16 21:17:35 +0600

    Interface

--- Java practice/Interface/AccountOperations.java
+++ Java practice/Interface/AccountOperations.java
@@ -1,3 +1,6 @@
-public class AccountOperations {
+public interface AccountOperations {
     
+    public void addAccount(Account ac1);
+    public void removeAccount(Account ac2);
+    public void showAccount();
 }
