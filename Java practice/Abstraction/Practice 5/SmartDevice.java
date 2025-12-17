d87999c92f488c4973d9debc7ab999d0c4b9a466
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-17 19:51:24 +0600

    Abstraction

--- Java practice/Abstraction/Practice 5/SmartDevice.java
+++ Java practice/Abstraction/Practice 5/SmartDevice.java
@@ -1,3 +1,31 @@
-public class SmartDevice {
-    
+public abstract class SmartDevice {
+    private String name;
+    public boolean isOn;
+
+    public SmartDevice(String name){
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void turnOn() {
+        boolean isOn = false;
+        if (isOn == true) {
+            System.out.println("Already On!");
+        } else {
+            System.out.println("Turn On.");
+        }
+    }
+
+    public void turnOff() {
+
+    }
+
+    public abstract String getStatus();
 }
