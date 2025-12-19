22909f0bc70ad3e83d2d8aa196ad845cc7824ed5
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-19 11:14:03 +0600

    showMessageDialog with 5 parameter

---
+++ Java Swing/MessageDialog/showMessageDialog3.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+public class showMessageDialog3 {
+    public static void main(String[] args) {
+        ImageIcon img = new ImageIcon("17969508.png");
+        JOptionPane.showMessageDialog(null,"Wrong Password","Enter password",JOptionPane.PLAIN_MESSAGE, img);
+    }
+}
