f75c2de97216971cbc7c8ddf8af9a0d034c39658
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-19 10:46:18 +0600

    showMessageDialog with 2 parameter

--- Java Swing/MessageDialog/showMessageDialog1.java
+++ Java Swing/MessageDialog/showMessageDialog1.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
 public class showMessageDialog1 {
-    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Wrong Message!");
+    }
 }
