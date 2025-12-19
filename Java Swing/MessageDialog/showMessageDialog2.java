a313987731114c833641df5e4415089afea85b20
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-19 10:49:57 +0600

    showMessageDialog with 4 parameter

---
+++ Java Swing/MessageDialog/showMessageDialog2.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+public class showMessageDialog2 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Wrong password!", "Warning",JOptionPane.WARNING_MESSAGE);
+    }
+}
