c84fade551ae41a684955b021780be613a6e8550
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-20 13:06:45 +0600

    JFrame with constructor

--- Java Swing/JFrame/JFrameDemo2.java
+++ Java Swing/JFrame/JFrameDemo2.java
@@ -1,3 +1,15 @@
-public class JFrameDemo2 {
-    
+import javax.swing.JFrame;
+
+public class JFrameDemo2 extends JFrame{
+
+    public JFrameDemo2(){
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(500, 100, 400, 500);
+        setTitle("JFrame Demo");
+        setResizable(false);
+    }
+    public static void main(String[] args) {
+        JFrameDemo2 frame = new JFrameDemo2();
+    }
 }
