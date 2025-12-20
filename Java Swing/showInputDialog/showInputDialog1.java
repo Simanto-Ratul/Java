bea875353a4ac94f4496712a294b490542e544bd
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-20 11:32:34 +0600

    showInputDialog with 2 parameter

--- Java Swing/showInputDialog/showInputDialog1.java
+++ Java Swing/showInputDialog/showInputDialog1.java
@@ -1,3 +1,8 @@
+import javax.swing.JOptionPane;
+
 public class showInputDialog1 {
-    
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+        JOptionPane.showMessageDialog(null, name + " Welcome to Java Swing.");
+    }
 }
