bf372a27ea1d88ac0fa5a840178469e2025eb3b7
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-20 14:21:37 +0600

    JFrame with backround and imageicon

--- Java Swing/JFrame ImageIcon/ImageIcon1.java
+++ Java Swing/JFrame ImageIcon/ImageIcon1.java
@@ -1,24 +1,27 @@
+import java.awt.*;
+
 import javax.swing.*;
-
-public class Imageicon extends JFrame {
-
+public class ImageIcon1 extends JFrame{
     private ImageIcon icon;
+    private Container c;
 
-    public Imageicon() {
+    public ImageIcon1(){
         initcomponents();
     }
 
-    public void initcomponents() {
+    public void initcomponents(){
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setBounds(500, 100, 400, 500);
+        this.setBounds(500,100,400,500);
         this.setResizable(false);
         this.setTitle("JFrame Demo");
         icon = new ImageIcon(getClass().getResource("bonfire.png"));
         this.setIconImage(icon.getImage());
+        c = this.getContentPane();
+        c.setBackground(Color.yellow);
     }
 
     public static void main(String[] args) {
-        Imageicon i1 = new Imageicon();
+        ImageIcon1 i2 = new ImageIcon1();
     }
 }
