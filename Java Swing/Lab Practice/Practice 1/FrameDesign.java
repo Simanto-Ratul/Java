8c0fef573d8cc288683af0a3bc43f6f07c957426
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-28 21:42:34 +0600

    Lab Practice

--- Java Swing/Lab Practice/Practice 1/FrameDesign.java
+++ Java Swing/Lab Practice/Practice 1/FrameDesign.java
@@ -1,6 +1,9 @@
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class FrameDesign extends JFrame {
+    private ImageIcon imageIcon1;
+
     public FrameDesign() {
         initComponents();
     }
@@ -10,5 +13,8 @@ public class FrameDesign extends JFrame {
         this.setVisible(true);
         this.setBounds(350,50,600,700);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        imageIcon1 = new ImageIcon(getClass().getResource("pumpkin.png"));
+        this.setIconImage(imageIcon1.getImage());
     }
 }
