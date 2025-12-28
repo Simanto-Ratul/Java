d97c6ac1b9773741bfce5251f48aba329e31d13d
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-28 21:48:45 +0600

    Learning java

--- Java Swing/Lab Practice/Practice 1/FrameDesign.java
+++ Java Swing/Lab Practice/Practice 1/FrameDesign.java
@@ -1,8 +1,11 @@
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class FrameDesign extends JFrame {
     private ImageIcon imageIcon1;
+    private Color color1, color2;
+    private Container c;
 
     public FrameDesign() {
         initComponents();
@@ -16,5 +19,10 @@ public class FrameDesign extends JFrame {
 
         imageIcon1 = new ImageIcon(getClass().getResource("pumpkin.png"));
         this.setIconImage(imageIcon1.getImage());
+
+        color1 = new Color(23, 46, 88);
+
+        c = this.getContentPane();
+        c.setBackground(color1);
     }
 }
