0791dce9c0289535657857b1d36847e34d64935b
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-29 18:56:53 +0600

    ActionListener Learning

---
+++ Java Swing/ActionListener/Practice 1/ActionListenerDemo.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ActionListenerDemo extends JFrame{
+    public static void main(String[] args) {
+        ActionListenerDemo a1 = new ActionListenerDemo();
+        a1.setVisible(true);
+    }
+}
