93e5a141983e1968d5e165982c2a899d56bdf407
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-31 08:57:22 +0600

    Login Frame

---
+++ Java Swing/LoginFrame/NewFrame.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class NewFrame extends JFrame {
+    private Container c;
+    private JLabel label1;
+
+    public NewFrame() {
+        this.setTitle("New Frame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(450, 100, 300, 400);
+        this.setResizable(false);
+
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.RED);
+
+        label1 = new JLabel("Hello Everyone!");
+        label1.setBounds(50, 50, 150, 50);
+        c.add(label1);
+
+    }
+
+    public static void main(String[] args) {
+        NewFrame f1 = new NewFrame();
+        f1.setVisible(true);
+    }
+}
