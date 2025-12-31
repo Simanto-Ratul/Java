9c0cfd8e8a1ee382776e4ab950bc71c5bbbcf9dc
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-31 19:14:36 +0600

    Multiplication Table Project

---
+++ Java Swing/Multiplication Table/Main.java
@@ -0,0 +1,6 @@
+public class Main extends MultiplicationTable{
+    public static void main(String[] args) {
+        MultiplicationTable t1 = new MultiplicationTable();
+        t1.setVisible(true);
+    }
+}
