7700c43f22b41c5ce643f4ff6f5998220f1b5dd8
Simanto Saha <sahasimanto.828@gmail.com>
2025-12-31 18:32:37 +0600

    JScrollPane

--- Java Swing/JScrollPane/JScrollPaneDemo.java
+++ Java Swing/JScrollPane/JScrollPaneDemo.java
@@ -1,3 +1,32 @@
-public class JScrollPaneDemo {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class JScrollPaneDemo extends JFrame {
+    private Container c;
+    private JTextArea textArea1;
+    private JScrollPane scrollPane1;
+
+    public JScrollPaneDemo() {
+        this.setTitle("JScrollPane");
+        this.setBounds(450, 100, 400, 400);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.MAGENTA);
+
+        textArea1 = new JTextArea();
+        // textArea1.setBounds(20,50,100,100);
+        textArea1.setForeground(Color.WHITE);
+        textArea1.setBackground(Color.DARK_GRAY);
+        textArea1.setLineWrap(true);
+        textArea1.setWrapStyleWord(true);
+        // c.add(textArea1);
+
+        scrollPane1 = new JScrollPane(textArea1);
+        scrollPane1.setBounds(20, 50, 100, 100);
+        c.add(scrollPane1);
+
+    }
 }
---
+++ Java Swing/JScrollPane/Main.java
@@ -0,0 +1,6 @@
+public class Main extends JScrollPaneDemo {
+    public static void main(String[] args) {
+        JScrollPaneDemo s1 = new JScrollPaneDemo();
+        s1.setVisible(true);
+    }
+}
