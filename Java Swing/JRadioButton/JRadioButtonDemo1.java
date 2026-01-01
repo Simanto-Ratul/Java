b0e94fa4e49044117b5f4b85e3db14e6b0d3b476
Simanto Saha <sahasimanto.828@gmail.com>
2026-01-01 18:57:03 +0600

    JRadioButton

--- Java Swing/JRadioButton/JRadioButtonDemo1.java
+++ Java Swing/JRadioButton/JRadioButtonDemo1.java
@@ -1,3 +1,33 @@
-public class JRadioButtonDemo1 {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class JRadioButtonDemo1 extends JFrame {
+    private Container c;
+    private JRadioButton button1, button2;
+
+    public JRadioButtonDemo1() {
+        this.setTitle("JRadioButton");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(450, 100, 400, 400);
+        this.setResizable(false);
+
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.ORANGE);
+
+        button1 = new JRadioButton("Male");
+        button1.setBounds(20, 50, 100, 50);
+        button1.setBackground(Color.ORANGE);
+        c.add(button1);
+
+        button2 = new JRadioButton("Female");
+        button2.setBounds(150, 50, 100, 50);
+        button2.setBackground(Color.ORANGE);
+        c.add(button2);
+    }
+
+    public static void main(String[] args) {
+        JRadioButtonDemo1 b1 = new JRadioButtonDemo1();
+        b1.setVisible(true);
+    }
 }
