7c26ce7dfdbe4674f893c17a78bdaee01cb7718b
Simanto Saha <sahasimanto.828@gmail.com>
2026-01-02 10:03:40 +0600

    JCheckBox Learning

--- Java Swing/JCheckBox/JCheckBoxDemo1.java
+++ Java Swing/JCheckBox/JCheckBoxDemo1.java
@@ -1,3 +1,41 @@
-public class JCheckBoxDemo1 {
-    
+import javax.swing.*;
+import java.awt.*;
+
+public class JCheckBoxDemo1 extends JFrame {
+    private Container c;
+    private JCheckBox physicsBox, chemistryBox, matCheckBox;
+    private Font f;
+
+    public JCheckBoxDemo1() {
+        this.setTitle("JCheckBox");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setBounds(450, 100, 400, 500);
+        this.setResizable(false);
+
+        f = new Font("Cambria", Font.BOLD, 14);
+
+        c = this.getContentPane();
+        c.setLayout(null);
+        c.setBackground(Color.CYAN);
+
+        physicsBox = new JCheckBox("Physics");
+        physicsBox.setBounds(30, 50, 80, 50);
+        physicsBox.setFont(f);
+        c.add(physicsBox);
+
+        chemistryBox = new JCheckBox("Chemistry");
+        chemistryBox.setBounds(130, 50, 100, 50);
+        chemistryBox.setFont(f);
+        c.add(chemistryBox);
+
+        matCheckBox = new JCheckBox("Math");
+        matCheckBox.setBounds(240, 50, 80, 50);
+        matCheckBox.setFont(f);
+        c.add(matCheckBox);
+    }
+
+    public static void main(String[] args) {
+        JCheckBoxDemo1 d1 = new JCheckBoxDemo1();
+        d1.setVisible(true);
+    }
 }
