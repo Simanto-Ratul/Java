0c964f9ee5701b8600fe2a8f99b439a57cbee621
Simanto Saha <sahasimanto.828@gmail.com>
2026-01-02 10:12:29 +0600

    JCheckBox

--- Java Swing/JCheckBox/JCheckBoxDemo1.java
+++ Java Swing/JCheckBox/JCheckBoxDemo1.java
@@ -5,6 +5,7 @@ public class JCheckBoxDemo1 extends JFrame {
     private Container c;
     private JCheckBox physicsBox, chemistryBox, matCheckBox;
     private Font f;
+    private ButtonGroup group;
 
     public JCheckBoxDemo1() {
         this.setTitle("JCheckBox");
@@ -13,6 +14,7 @@ public class JCheckBoxDemo1 extends JFrame {
         this.setResizable(false);
 
         f = new Font("Cambria", Font.BOLD, 14);
+        group = new ButtonGroup();
 
         c = this.getContentPane();
         c.setLayout(null);
@@ -21,17 +23,24 @@ public class JCheckBoxDemo1 extends JFrame {
         physicsBox = new JCheckBox("Physics");
         physicsBox.setBounds(30, 50, 80, 50);
         physicsBox.setFont(f);
+        physicsBox.setBackground(Color.CYAN);
         c.add(physicsBox);
 
         chemistryBox = new JCheckBox("Chemistry");
         chemistryBox.setBounds(130, 50, 100, 50);
         chemistryBox.setFont(f);
+        chemistryBox.setBackground(Color.CYAN);
         c.add(chemistryBox);
 
         matCheckBox = new JCheckBox("Math");
         matCheckBox.setBounds(240, 50, 80, 50);
         matCheckBox.setFont(f);
+        matCheckBox.setBackground(Color.CYAN);
         c.add(matCheckBox);
+
+        group.add(physicsBox);
+        group.add(chemistryBox);
+        group.add(matCheckBox);
     }
 
     public static void main(String[] args) {
