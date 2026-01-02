227c71b5791151cc0dca0c44b842af356c26522e
Simanto Saha <sahasimanto.828@gmail.com>
2026-01-02 09:15:08 +0600

    JRadioButton with ActionListener

--- Java Swing/JRadioButton/JRadioButtonDemo1.java
+++ Java Swing/JRadioButton/JRadioButtonDemo1.java
@@ -20,7 +20,7 @@ public class JRadioButtonDemo1 extends JFrame {
         c.setLayout(null);
         c.setBackground(Color.ORANGE);
 
-        font = new Font("Cambria",Font.BOLD,16);
+        font = new Font("Cambria", Font.BOLD, 16);
         buttonGroup = new ButtonGroup();
 
         button1 = new JRadioButton("Male");
@@ -28,9 +28,9 @@ public class JRadioButtonDemo1 extends JFrame {
         button1.setBackground(Color.ORANGE);
         button1.setFont(font);
         button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 String s = button1.getText();
-                if(s.equals("Male")){
+                if (s.equals("Male")) {
                     textArea1.append("You have selected male.\n");
                 }
             }
@@ -42,9 +42,9 @@ public class JRadioButtonDemo1 extends JFrame {
         button2.setBackground(Color.ORANGE);
         button2.setFont(font);
         button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e){
+            public void actionPerformed(ActionEvent e) {
                 String s = button2.getText();
-                if(s.equals("Female")){
+                if (s.equals("Female")) {
                     textArea1.append("You have selected female.\n");
                 }
             }
@@ -55,7 +55,7 @@ public class JRadioButtonDemo1 extends JFrame {
         buttonGroup.add(button2);
 
         textArea1 = new JTextArea();
-        textArea1.setBounds(50,100,200,200);
+        textArea1.setBounds(50, 100, 200, 200);
         textArea1.setFont(font);
         textArea1.setBackground(Color.DARK_GRAY);
         textArea1.setForeground(Color.WHITE);
